f10726de3075cc9d1429397125acea7cf24e2f41
Ivan Ashikhmin <sushkoos@gmail.com>
2023-10-12 13:40:10 +0400

    Регистрация моделей в админке

--- books_app/admin.py
+++ books_app/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.AuthorModel)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'dob',)
+    search_fields = ('name',)
+
+
+@admin.register(models.BookModel)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cover_preview', 'author', 'publication_date')
+    search_fields = ('title', 'author')
+
+    def cover_preview(self, obj):
+        """ Метод, добавляющий превью обложки на странице в админке """
+        return format_html(f'<img src="{obj.book_cover.url}" width="100"/>')
+
+    cover_preview.short_description = 'Обложка'
--- library/settings.py
+++ library/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-_apzwotj3d9ku=y*(1+rkptjry&=t_ahf#pc%(_p!s8oc92o9m
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -69,7 +67,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'library.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -79,7 +76,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -99,7 +95,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -111,11 +106,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 
 MEDIA_URL = 'media/'
