120dab47a18b611a5510bf5570a97cb8d519dd3b
Ivan Ashikhmin <sushkoos@gmail.com>
2023-10-13 21:40:33 +0400

    Добавил API и обновил README.md

--- api_app/urls.py
+++ api_app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from api_app import views
 
 urlpatterns = [
-    path('all_books/', views.AllBooksView.as_view(), name='all_books'),
+    path('all_books/', views.AllBooksView.as_view(), name='get_all_books'),
     path('get_book/<int:pk>', views.BookView.as_view(), name='get_book'),
 ]
--- books_app/models.py
+++ books_app/models.py
@@ -53,5 +53,9 @@ class CommentModel(models.Model):
     comment = models.TextField(verbose_name='Комментарий')
     created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
     def __str__(self):
         return f'Комментарий от {self.user} к книге {self.book}'
